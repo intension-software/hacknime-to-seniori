1c8222837defa797c62c949941e52b8e9084baa1
Svaagi <jozefsvagerkom5@gmail.com>
2022-12-10 14:39:55 +0100

    profile

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,17 +1,34 @@
 import { FC } from 'react'
 import Header from '../components/Header';
-import Tips from '../components/Tips';
-import Tutorials from '../components/Tutorials';
-import Forum from '../components/Forum';
-import storage from '../utils/storage';
+import './Account.css';
 
 const Account: FC = () => {
-  const user = storage.getItem('user');
-
   return (
     <div className="Account">
-      <Header />
-      <img style={{width: "100%", height: "100%"}} src="./images/user.png"/>
+      <div className='AccHeader'>
+        <div style={{display: "flex", alignItems: "center"}}>
+          <img className='img' src="./images/profile.png" />
+          <div className='AccTitle'>Jozef Švagerko    <img width="45px" height="45px"src="./images/star.png" />4.1</div>
+        </div>
+        <button className='Button2'>Kontaktovať</button>
+      </div>
+      <div className='AccBody'>
+        <div>
+          <div className='AccTitle2'>Oblasť</div>
+          <div className='AccBadge'>Poprad</div>
+          <img width="500px" height="250px" src="./images/map.png"/>
+        </div>
+        <div>
+          <div className='AccTitle2'>Krátke BIO</div>
+          <div>Jozef Švagerko je technicky zdatný človek, ktorý dokáže rýchlo pochopiť aj náročnejšie technické záležitosti. Je obľúbený medzi staršími ľuďmi, s ktorými si rád zakamaráti a trávi s nimi svoj voľný čas. Jeho netrpezlivosť však niekedy prekáža a môže viesť k nedorozumeniam. Aj napriek tomu je veľmi spoločenský a rád trávi čas s ľuďmi. Jozef je nižšieho vzrastu, čo mu však nijako nebráni v tom, aby dosiahol veľké veci.</div>
+          <div className='AccTitle2'>Expertíza</div>
+          <div style={{display: "flex", width:"500px", gap:"1rem", marginTop: "1rem"}}>
+            <div className='Badge'>Online Komunikácia</div>
+            <div className='Badge'>Cestovné poriadky</div>
+            <div className='Badge'>Správy</div>
+          </div>
+        </div>  
+        </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,6 @@ const Home: FC = () => {
 
   return (
     <div className="Home">
-      <Header />
       {user ? <Forum /> : null}
       <Tips />
       <Tutorials />
